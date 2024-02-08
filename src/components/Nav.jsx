0a3f1bb5a984fd2c7cec4a9b818d59497abd08fd
Sergey UP <sergey10goals@gmail.com>
2024-02-08 23:26:34 +0200

    fixed header position to absolute

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
-    <header className="padding-x py-8 z-10 w-full">
+    <header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img src={headerLogo}
