c2a54aa41145d6150749194b2ef2836824c874a0
Sergey UP <sergey10goals@gmail.com>
2024-02-09 19:09:54 +0200

    updated Button component: added fullWidth prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
-const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) => {
   return (
     <button className={`
     flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+    ${fullWidth && 'w-full'}
     ${backgroundColor ?
         `${backgroundColor} ${borderColor} ${textColor}` :
         `bg-coral-red border-coral-red text-white`}
