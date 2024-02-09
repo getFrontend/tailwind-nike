c5f054a064afa1daa8d6e7d6e59dee8cbfeedbad
Sergey UP <sergey10goals@gmail.com>
2024-02-09 21:57:20 +0200

    updated Nav component: added Sign in & Explore now links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,19 +7,26 @@ const Nav = () => {
     <header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
-          <img src={headerLogo}
+          <img
+            className="m-0 w-[129px] h-[29px]"
+            src={headerLogo}
             alt="logo image"
-            width={130}
+            width={129}
             height={29}
           />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+        <ul className="flex-1 flex justify-center items-center gap-16 max-xl:gap-8 max-lg:hidden">
           {navLinks.map((item) => (
             <li key={item.label}>
               <a className="font-montserrat leading-normal text-lg text-slate-gray" href={item.href}>{item.label}</a>
             </li>
           ))}
         </ul>
+        <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+          <a className="hover:text-coral-red" href="#!">Sign in</a>
+          <span>/</span>
+          <a className="hover:text-coral-red" href="#!">Explore now</a>
+        </div>
         <div className="hidden max-lg:block">
           <img src={hamburger}
             alt="mobile menu"
