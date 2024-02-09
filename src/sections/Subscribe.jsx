2bba9b78f585e49a31b62071120ee0c78db12472
Sergey UP <sergey10goals@gmail.com>
2024-02-09 19:10:12 +0200

    finished Subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,20 @@
+import Button from "../components/Button"
+
 const Subscribe = () => {
   return (
-    <div>
-
+    <div id="contact-us"
+      className="max-container flex justify-between items-center gap-10 max-lg:flex-col">
+      <h3 className="text-4xl font-palanquin font-bold leading-[68px] lg:max-w-md">
+        Sign Up for
+        <span className='text-coral-red'> Updates </span>
+        & Newsletter
+      </h3>
+      <div className="w-full p-2.5 flex gap-5 items-center max-sm:flex-col lg:max-w-[40%] lg:gap-2 sm:border sm:border-slate-gray rounded-full">
+        <input className="input" type="text" placeholder="subscribe@nike.com" />
+        <div className="flex flex-shrink-0 items-center max-sm:w-full max-sm:justify-end">
+          <Button label="Sign up" fullWidth />
+        </div>
+      </div>
     </div>
   )
 }
