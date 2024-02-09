066a137ebb9fac4f03b9e2b81be9e1eb1c9df507
Sergey UP <sergey10goals@gmail.com>
2024-02-09 17:03:28 +0200

    updated Button component: added color props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,11 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border border-coral-red font-montserrat text-lg leading-none bg-coral-red text-white rounded-full">
+    <button className={`
+    flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+    ${backgroundColor ?
+        `${backgroundColor} ${borderColor} ${textColor}` :
+        `bg-coral-red border-coral-red text-white`}
+    `}>
       {label}
       {iconURL && <img src={iconURL}
         alt="button icon"
