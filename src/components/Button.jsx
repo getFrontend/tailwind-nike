ee686328a94dcc94205e200e911a647bb2bd0539
Sergey UP <sergey10goals@gmail.com>
2024-02-09 15:40:54 +0200

    updated Button component: added check for icon if it is needed in the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,10 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border border-coral-red font-montserrat text-lg leading-none bg-coral-red text-white rounded-full">
       {label}
-      <img src={iconURL}
+      {iconURL && <img src={iconURL}
         alt="button icon"
         className="ml-2 rounded-ful w-5 h-5"
-      />
+      />}
     </button>
   )
 }
